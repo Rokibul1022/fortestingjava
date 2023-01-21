36fd70d8ad572ae421faf39cbeebc87b8b6bacf1
Rokibul1022 <hrrokibul2020@gmail.com>
2023-01-21 14:43:10 +0600

    addng from laptop

---
+++ testing java/demo.java
@@ -0,0 +1,10 @@
+public class demo {
+    public static void main(String[] args){
+        System.out.println("Id: 101,");
+        System.out.println("Title: Iphone 15,");
+        System.out.println("price: 1895 euros,");
+        System.out.println("discription: perfect product with the best image quality,");
+        System.out.println("catagory: phone,");
+
+    }
+}
